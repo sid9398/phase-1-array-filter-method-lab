839a27e5788e71192cc5172ce4198cb9ac3a37fa
SidneyKing <sid8121@gmail.com>
2023-08-27 16:21:12 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+function findMatching(drivers, search) {
+    return drivers.filter(driver => driver.toLowerCase().includes(search.toLowerCase()))
+}
+function fuzzyMatch(drivers, query) {
+    const matchedDrivers = [];
+    const lowercaseQuery = query.toLowerCase();
+  
+    drivers.forEach(driver => {
+      const lowercaseDriverName = driver.slice(0, query.length).toLowerCase();
+      if (lowercaseDriverName.startsWith(lowercaseQuery)) {
+        matchedDrivers.push(driver);
+      }
+    });
+  
+    return matchedDrivers;
+  }
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
